325ff5d004f606e534960b023f71e07848ec81b5
Dom Clayton <dominic@booandjoel.co.uk>
2021-07-09 18:37:30 +0100

    Updated DevOps script and documentation

--- AzureDevOps/Azure-DevOps-Service-Connection.ps1
+++ AzureDevOps/Azure-DevOps-Service-Connection.ps1
@@ -11,6 +11,7 @@
 
     .EXAMPLE
         Azure-DevOps-Service-Connection.ps1
+        Azure-DevOps-Service-Connection.ps1 -servicePrincipalName PipelineSP
 
 #>
 
@@ -18,24 +19,35 @@
 #Requires -Modules @{ModuleName="AzureAD"; ModuleVersion="2.0.2.128"}
 #Requires -Modules @{ModuleName="Az.Accounts"; ModuleVersion="2.2.3"}
 
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$false)]
+    [string]$servicePrincipalName=""
+)
 
+If ($servicePrincipalName -eq ""){
+    $servicePrincipalName = "PipelineDeployment"
+}
+
+Write-Output -InputObject ('Connecting from Azure AD')
 # Connect to Azure AD
 Connect-AzureAD | Out-Null
+Write-Output -InputObject ('Connected from Azure AD')
 
 $AADId = (Get-AzureADTenantDetail).ObjectId
 
 # Create an Azure AD application which the service principal will use, check
 # if it exists first.
-If (!(Get-AzureADApplication -SearchString "PipelineDeployment")){
-    $aadApp = New-AzureADApplication -DisplayName "PipelineDeployment" `
+If (!(Get-AzureADApplication -SearchString $servicePrincipalName)){
+    $aadApp = New-AzureADApplication -DisplayName $servicePrincipalName `
         -Homepage "https://localhost" `
         -ReplyUrls "https://localhost" `
 } Else {
-    $aadApp = Get-AzureADApplication -SearchString "PipelineDeployment"
+    $aadApp = Get-AzureADApplication -SearchString $servicePrincipalName
 }
 
 # Create the service principal for the pipeline if it doesn't already exist
-if (!(Get-AzureADServicePrincipal -SearchString "PipelineDeployment")){
+if (!(Get-AzureADServicePrincipal -SearchString $servicePrincipalName)){
 
     # Create a password
     $aadAppPwd = New-AzureADApplicationPasswordCredential -ObjectId $aadApp.ObjectId -CustomKeyIdentifier "ConnectFromAzureDevOps"
@@ -44,7 +56,7 @@ if (!(Get-AzureADServicePrincipal -SearchString "PipelineDeployment")){
 
 } Else {
 
-    $aadSp = Get-AzureADServicePrincipal -SearchString "PipelineDeployment"
+    $aadSp = Get-AzureADServicePrincipal -SearchString $servicePrincipalName
 
 }
 
@@ -87,29 +99,41 @@ Catch {
     Write-Warning $Error[0].Exception.Message
 }
 
-
+Write-Output -InputObject ('Disconnecting from Azure AD')
 # Disconnect from AzureAD
 Disconnect-AzureAD | Out-Null
 
-Write-Output -InputObject ('Disconnecting from Azure AD')
+Write-Output -InputObject ('Disconnected from Azure AD')
 Start-Sleep -Seconds 15
 
+Write-Output -InputObject ('Connecting to Azure')
 # Connect to Azure
 Connect-AzAccount | Out-Null
+Write-Output -InputObject ('Connected to Azure')
+
+# Select the subscription from the menu
+$Subscription = Get-AzSubscription | Select-Object Name, SubscriptionId `
+| Out-GridView -Title "Select the subscription you want to deploy to" -PassThru
+Set-AzContext -SubscriptionId $Subscription.SubscriptionId | Out-Null
+
+$subId = (Get-AzContext).Subscription.Id
 
 # Grant the Service Principal owner rights on the subscription. Owner is required to apply RBAC from the DevOps Pipeline.
 Try {
-    New-AzRoleAssignment -ObjectId $aadSp.objectId -RoleDefinitionName "owner" -Scope (-join("/subscriptions/",(Get-AzContext).Subscription.Id)) -ErrorAction Stop | Out-Null
+    New-AzRoleAssignment -ObjectId $aadSp.objectId -RoleDefinitionName "owner" -Scope (-join("/subscriptions/", $subId)) -ErrorAction Stop | Out-Null
 }
 Catch {
     Write-Warning $Error[0].Exception.Message
 }
 
-
+Write-Output -InputObject ('Disconnecting from Azure')
 Disconnect-AzAccount | Out-Null
+Write-Output -InputObject ('Disconnected from Azure AD')
 
 ### Output information ###
 Write-Output -InputObject ('The service principal Id: {0}' -f $aadSp.AppId)
 Write-Output -InputObject ('The service principal key: {0}' -f $aadAppPwd.value)
 Write-Output -InputObject ('The Azure AD tenant Id: {0}' -f $aadId)
+Write-Output -InputObject ('The Azure Subscription Id: {0}' -f $subId)
 Write-Output -InputObject ('The API permissions granted within this script will more than likely need approving by an administrator within the portal')
+#>
--- AzureDevOps/readMe.md
+++ AzureDevOps/readMe.md
@@ -2,10 +2,29 @@
 
 [Home](../readme.md)
 
-The PowerShell script in this folder can be used to grant the correct access permissons to an AAD tenant and subscription. The script takes no parameters - maybe it should - and simply creates a application registration and service principal in Azure AD and finally spits out an Service Principal Id, Service Principal Key, and Azure AD tenant Id. 
+The PowerShell script: Azure-DevOps-Service-Connection.ps1 creates an application registration and service principal in Azure AD, then spits out the Service Principal Id, Service Principal Key, Azure AD tenant Id, and Subscription Id of the subscription you select.
+
+You can specify your own service principal name or accept the default PipelineDeployment e.g. 
+
+Azure-DevOps-Service-Connection.ps1
+
+or 
+
+Azure-DevOps-Service-Connection.ps1 -servicePrincipalName Something
+
+You can use these to populate the Service Connection information within Azure 
+DevOps.  
 
 ![Service Principal](https://stdevt4z3f7au4f3xe.blob.core.windows.net/images/CreateServicePrincipal.PNG)
 
 This can be used within Azure DevOps to create a Service Connection. You'll also need the subscription Id too.
 
 ![Service Connection](https://stdevt4z3f7au4f3xe.blob.core.windows.net/images/ServiceConnection.PNG)
+
+I've added several API permissions to the service principal which allows pipeline deployment to create another service principal - this is useful when deploying an Azure Automation RunAs Account. 
+
+![API Permissions](https://stdevt4z3f7au4f3xe.blob.core.windows.net/images/APIPermissions.PNG)
+
+The service principal is also granted owner on the subscription to enable the pipeline deployment to assign RBAC.
+
+![Service Principal RBAC](https://stdevt4z3f7au4f3xe.blob.core.windows.net/images/SPRBAC.PNG)
